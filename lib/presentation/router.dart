b3ca12f752951d707ffe803b1cec0267b344d60e
Mahbubur Rahman <rifat0153@gmail.com>
2021-11-29 20:15:09 +0000

    removed unnecessary files

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,18 +1,13 @@
-import 'package:cubit_test/cubit/product_cubit.dart';
-import 'package:cubit_test/data/models/product.dart';
-import 'package:cubit_test/data/post_network_service.dart';
-import 'package:cubit_test/data/product/product_network_service.dart';
-import 'package:cubit_test/data/product/product_repository.dart';
-import 'package:cubit_test/presentation/screens/product/product_detail_screen.dart';
-import 'package:cubit_test/presentation/screens/product/product_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:cubit_test/cubit/post_cubit.dart';
-import 'package:cubit_test/data/post_repository.dart';
-import 'package:cubit_test/presentation/screens/post_detail_screen.dart';
-import 'package:cubit_test/presentation/screens/post_screen.dart';
+import '../cubit/product_cubit.dart';
+import '../data/models/product.dart';
+import '../data/product/product_network_service.dart';
+import '../data/product/product_repository.dart';
+import 'screens/product/product_detail_screen.dart';
+import 'screens/product/product_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -39,24 +34,12 @@ class AppRouter {
           ),
         );
 
-      case "/a":
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-            create: (BuildContext context) =>
-                PostCubit(postRepository: PostRepository(postNetworkService: PostNetworkService())),
-            child: PostScreens(),
-          ),
-        );
-
-      case "/post_detail":
-        return MaterialPageRoute(builder: (_) => PostDetailScreen());
-
       default:
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (BuildContext context) =>
-                PostCubit(postRepository: PostRepository(postNetworkService: PostNetworkService())),
-            child: PostScreens(),
+                ProductCubit(repository: ProductRepository(service: ProductNetworkService())),
+            child: const ProductScreen(),
           ),
         );
     }
